fcd70d96deebd289eed854fb8e7fa57087036a77
Your Name <tonnam_t@windowslive.com>
2020-06-17 16:45:12 +0700

    Chatlog

--- src/MESSAGE/messagescene.gd
+++ src/MESSAGE/messagescene.gd
@@ -1,16 +1,18 @@
 extends Control
 
-
-
+onready var namelabel = $MessagePanel/HSplitContainer/PersonalChat/CHAT/NAME
+onready var messagecontainer = $MessagePanel/HSplitContainer/PersonalChat/CHAT/Messagelog
+onready var choosecontainer = $MessagePanel/HSplitContainer/PersonalChat/CHAT/Choose
+onready var namecontainer = $MessagePanel/HSplitContainer/Namelist/Namecontainer
 func setTitle(title):
-	$NAME.text = title
+	namelabel.text = title
 	
 
 func message_receive(message):
 	var chat = Label.new()
 	chat.text = message
 	chat.align = Label.ALIGN_LEFT
-	$Messagelog.add_child(chat)
+	messagecontainer.add_child(chat)
 	
 		
 	
@@ -18,7 +20,7 @@ func message_send(message):
 	var chat = Label.new()
 	chat.text = message
 	chat.align = Label.ALIGN_RIGHT
-	$Messagelog.add_child(chat)
+	messagecontainer.add_child(chat)
 
 
 func choose(message : Array):
@@ -27,20 +29,31 @@ func choose(message : Array):
 		k.text = i
 		k.align = Label.ALIGN_RIGHT
 		k.connect("pressed",self,"delete",[i])
-		$Choose.add_child(k)
+		choosecontainer.add_child(k)
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 func delete(message):
 	message_send(message)
-	for e in $Choose.get_children():
-		$Choose.remove_child(e)
+	for e in choosecontainer.get_children():
+		choosecontainer.remove_child(e)
 		e.queue_free()
-	
-	
-
+		
+func putnamelist(namelist : Array):
+	for e in namelist:
+		var k = Button.new()
+		k.text = e
+		k.connect("pressed",self,"getchat",[e])
+		namecontainer.add_child(k)
 # Called when the node enters the scene tree for the first time.
+
+func getchat(name:String):
+	setTitle(name)
+
 func _ready():
+	$MessagePanel/AppControl.app = self
+	putnamelist(["OjadeO","Kirkpig","Naem"])
+
 	message_receive("A")
 	message_send("B")
 	message_receive("C")
